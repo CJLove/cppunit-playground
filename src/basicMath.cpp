cf4203baf6cd8f8655ac2d5646526af91d55d390
Chris Love <christopherlove68@gmail.com>
2020-07-17 19:34:40 -0700

    Add files

---
+++ src/basicMath.cpp
@@ -0,0 +1,11 @@
+#include "basicMath.h"
+
+int BasicMath::Addition(int x, int y)
+{
+   return (x + y);
+}
+
+int BasicMath::Multiply(int x, int y)
+{
+   return (x * y);
+}
---
+++ src/basicMath.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class BasicMath
+{
+public:
+   int Addition(int x, int y);
+   int Multiply(int x, int y);
+
+};
